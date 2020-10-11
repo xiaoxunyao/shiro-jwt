bbee33a4ed735cdedee92f471958c9c7f167fa26
HCY <2414776182@qq.com>
2020-10-11 21:39:37 +0800

    shiro+jwt的SQL

--- src/main/java/com/inet/codebase/utlis/ShiroMd5Utlis.java
+++ src/main/java/com/inet/codebase/utlis/ShiroMd5Utlis.java
@@ -11,12 +11,26 @@ import org.apache.shiro.util.ByteSource;
  */
 public class ShiroMd5Utlis {
 
+    /**
+     * 加密盐
+     */
     private static final String SIGN = "HCY";
 
+    /**
+     * 加密算法
+     */
     private static final String ALG = "MD5";
 
+    /**
+     * 散列次数
+     */
     private static final int HASH = 1024;
 
+    /**
+     * 创建加密过后的字符串
+     * @param password
+     * @return
+     */
     public static String encryption(String password){
         return new SimpleHash(ALG, password, ByteSource.Util.bytes(SIGN), HASH).toString();
     }
